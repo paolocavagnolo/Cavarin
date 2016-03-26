a20d5d1628e3503571a3a5d79524151344e05b9f
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-03-26 23:14:17 +0100

    go

--- Silvia.ino
+++ Silvia.ino
@@ -30,7 +30,6 @@ void noteOff(byte channel, byte pitch, byte velocity) {
 
 //MUSIC NOTES ARRAY
 int firstNote = 0;
-int firstNote_o = 0;
 int musicNotesArray[NOTE_TOTAL_NUMBER];
 
 int minor_pentatonic[]  = {0, 3, 5, 7, 10, 12, 15, 17, 19, 22, 24, 27, 29, 31};
@@ -45,6 +44,7 @@ int* multiScales[POTSCALE_TOTAL_NUMBER] = {minor_pentatonic, major_pentatonic, m
 
 //Definition of the 3 analog measure we need, as interval, in such a way we can apply the isteresys function instead of the MAP function
 int intervalDistance = 0;
+int intervalDistance_o=0;
 int intervalPotNote = 0;
 int intervalPotNote_o=0;
 int intervalPotScale = 0;
@@ -144,16 +144,16 @@ void loop()
 
   //read intervals
   sma_vect = sma_filter(vect,histDistance);
+  intervalDistance_o = intervalDistance;
   intervalDistance = istRead(intervalDistance, sma_vect, thresholdDistance, DIST_MAX, DIST_MIN, 2, NOTE_TOTAL_NUMBER);
 
   intervalPotNote_o = intervalPotNote;
   intervalPotNote = istRead(intervalPotNote, analogRead(1), thresholdPotNote, 678, 0, 2, POTNOTE_TOTAL_NUMBER);
 
   intervalPotScale_o = intervalPotScale;
-  intervalPotScale = istRead(intervalPotScale, analogRead(0), thresholdPotScale, 678, 0, 2, POTSCALE_TOTAL_NUMBER);
+  intervalPotScale = istRead(intervalPotScale, (int)fscale(0, 678, 0, 678, map(analogRead(0), 678, 0, 0, 678), 5), thresholdPotScale, 678, 0, 2, POTSCALE_TOTAL_NUMBER);
 
   //intervals to values
-  firstNote_o = firstNote;
   firstNote = 24 + intervalPotNote;
 
   for (int c = 0; c < NOTE_TOTAL_NUMBER; c++) {
@@ -162,27 +162,92 @@ void loop()
 
   //change firstnote
   if (intervalPotNote_o != intervalPotNote) {
-    noteOff(1, firstNote_o, 125);
     noteOn(1, firstNote, 125);
     MidiUSB.flush();
-    delay(50);
+    delay(100);
+    noteOn(1, firstNote, 125);
+    MidiUSB.flush();
+
   }
 
   //change scale
   if (intervalPotScale_o != intervalPotScale) {
     noteOn(1, musicNotesArray[0], 125);
     MIDIUSB.flush();
+    delay(100);
     for (int p = 1; p < NOTE_TOTAL_NUMBER; p++){
-      delay(100);
       noteOff(1, musicNotesArray[p-1], 125);
       noteOn(1, musicNotesArray[p], 125);
       MIDIUSB.flush();
+      delay(100);
     }
-    noteOff(1, musicNotesArray[NOTE_TOTAL_NUMBER], 125);
+    noteOff(1, musicNotesArray[NOTE_TOTAL_NUMBER-1], 125);
     MIDIUSB.flush();
     delay(100);
+  }
+  /*
+  //play
+  if ((intervalDistance_o > -1) && (intervalDistance > -1)){
+    noteOn(1, musicNotesArray[intervalDistance], 125);
+    if (intervalDistance_o != intervalDistance) {
+      noteOff(1, musicNotesArray[intervalDistance_o], 125);
+      noteOn(1, musicNotesArray[intervalDistance], 125);
+      MidiUSB.flush();
+      delay(100);
+    }
+  }*/
 
+}
+
+float fscale( float originalMin, float originalMax, float newBegin, float
+              newEnd, float inputValue, float curve) {
+
+  float OriginalRange = 0;
+  float NewRange = 0;
+  float zeroRefCurVal = 0;
+  float normalizedCurVal = 0;
+  float rangedValue = 0;
+  boolean invFlag = 0;
+
+  if (curve > 10) curve = 10;
+  if (curve < -10) curve = -10;
+
+  curve = (curve * -.1) ; // - invert and scale - this seems more intuitive - postive numbers give more weight to high end on output
+  curve = pow(10, curve); // convert linear scale into lograthimic exponent for other pow function
+
+  // Check for out of range inputValues
+  if (inputValue < originalMin) {
+    inputValue = originalMin;
+  }
+  if (inputValue > originalMax) {
+    inputValue = originalMax;
   }
 
+  // Zero Refference the values
+  OriginalRange = originalMax - originalMin;
 
+  if (newEnd > newBegin) {
+    NewRange = newEnd - newBegin;
+  }
+  else
+  {
+    NewRange = newBegin - newEnd;
+    invFlag = 1;
+  }
+
+  zeroRefCurVal = inputValue - originalMin;
+  normalizedCurVal  =  zeroRefCurVal / OriginalRange;   // normalize to 0 - 1 float
+
+  // Check for originalMin > originalMax  - the math for all other cases i.e. negative numbers seems to work out fine
+  if (originalMin > originalMax ) {
+    return 0;
+  }
+  if (invFlag == 0) {
+    rangedValue =  (pow(normalizedCurVal, curve) * NewRange) + newBegin;
+  }
+  else     // invert the ranges
+  {
+    rangedValue =  newBegin - (pow(normalizedCurVal, curve) * NewRange);
+  }
+  return rangedValue;
 }
