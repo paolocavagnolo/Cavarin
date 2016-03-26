d3214d4e371fa97b99e1ee8bff72b8726ac940a0
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2016-03-26 18:32:57 +0100

    go

--- Silvia.ino
+++ Silvia.ino
@@ -1,55 +1,57 @@
 
-#define NOTE_TOTAL_NUMBER 5   //min 1 max 14
-#define SCALE_TOTAL_NUMBER 5  //min max
-#define DIST_MIN 50           //cm of a bottom dead bound
-#define DIST_MAX 100          //cm of the upper limit
-#define IST 2                 //accuracy of the isteresys cycle on the 'distance to note interval'
+#define NOTE_TOTAL_NUMBER 5           //min 1 max 14
+#define SCALE_TOTAL_NUMBER 5          //min max
+#define FIRSTNOTE_TOTAL_NUMBER 36     //firstnote pot
+#define DIST_MIN 50                   //cm of a bottom dead bound
+#define DIST_MAX 100                  //cm of the upper limit
+#define IST 2                         //accuracy of the isteresys cycle on the 'distance to note interval'
 
-//Definition of a function
-int istRead(int inputValue, int inputMax, int inputMin, int ist, int intervalTotal){
+int thresholdDistance[NOTE_TOTAL_NUMBER];
+int thresholdPotNote[FIRSTNOTE_TOTAL_NUMBER];
+int thresholdPotScale[SCALE_TOTAL_NUMBER];
 
-  if ((inputValue < inputMax) && (input > inputMin)) {
+int intervalDistance;
+int intervalPotNote;
+int intervalPotScale;
 
-    if ((inputValue < threshold[0] + ist)) {
+int istRead(int inputValue, int thresholdArray[], int thresholdArrayNum, int inputMax, int inputMin, int ist, int intervalTotal){
+
+  for (int z=0; z < thresholdArrayNum; z++) {
+    thresholdArray[z] = (z + 1) * (inputMax - (inputMin + 2)) / intervalTotal + inputMin;
+    if (z == interval) {
+      thresholdArray[z] += ist;
+    }
+  }
+
+  if ((inputValue <= inputMax) && (inputValue >= inputMin)) {
+    if (inputValue < thresholdArray[0]) {
       interval = 0;
     }
-
-    for (int k = 1; k < intervalTotal; k++) {
-      if ((inputValue >= (threshold[k - 1] + ist)) && (vect < (threshold[k] + ist))) {
-        interval = k;
+    else {
+      for (int k = 1; k < thresholdArrayNum; k++) {
+        if ((inputValue >= thresholdArray[k-1]) && (inputValue < thresholdArray[k])) {
+          interval = k;
+        }
       }
     }
+  }
+  else {
+    return -1;
+  }
 
-    return interval;
-}
-
-void noteOn(byte channel, byte pitch, byte velocity) {
-  MIDIEvent noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MIDIUSB.write(noteOn);
-}
-
-void noteOff(byte channel, byte pitch, byte velocity) {
-  MIDIEvent noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MIDIUSB.write(noteOff);
-}
-
-void controlChange(byte channel, byte control, byte value) {
-  MIDIEvent event = {0x0B, 0xB0 | channel, control, value};
-  MIDIUSB.write(event);
 }
 
 void setup()
 {
-  //initialize ist[]
-  for (int i = 0; i < NOTE_TOTAL_NUMBER; i++) {
-    ist[i] = 0;
+  for (int i=0; i < NOTE_TOTAL_NUMBER; i++){
+    thresholdDistance[i] = 0;
   }
-
-  //threshold set
-  for (int i = 0; i < NOTE_TOTAL_NUMBER; i++) {
-    threshold[i] = (i + 1) * (DIST_MAX - (DIST_MIN + 2)) / NOTE_TOTAL_NUMBER + (DIST_MIN + 3);
+  for (int ii=0; i < FIRSTNOTE_TOTAL_NUMBER; i++){
+    thresholdPotNote[ii] = 0;
   }
-
+  for (int iii=0; i < SCALE_TOTAL_NUMBER; i++){
+    thresholdPotScale[iii] = 0;
+  }
 }
 
 void loop()
